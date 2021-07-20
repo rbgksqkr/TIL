ea63dade83037f9d934e2fefa9b92938d8887e66
ghenmaru <63959171+rbgksqkr@users.noreply.github.com>
2021-07-20 23:42:58 +0900

    구현

--- 2021_07/keypad.py
+++ 2021_07/keypad.py
@@ -1,34 +1,39 @@
 def solution(numbers, hand):
-    # 순서대로 누를 번호가 담긴 배열 numbers, 왼손잡이인지 오른손잡이인 지를 나타내는 문자열 hand
-    # 각 번호를 누른 엄지손가락이 왼손인 지 오른손인 지를 나타내는 연속된 문자열 형태로 return
     answer = ''
-    left, right = 10, 12
+    left, right = (0,0), (2,0)
+    left_key = [1, 4, 7]
+    right_key = [3, 6, 9]
+    # 0~9의 좌표
+    location = [(1,0), (0,3), (1,3), (2,3), (0,2), (1,2), (2,2), (0,1), (1,1), (2,1)]
     
     for i in range(len(numbers)):
-        if numbers[i] == 1 or numbers[i] == 4 or numbers[i] == 7:
-            left = numbers[i]
+        if numbers[i] in left_key:
+            left = location[numbers[i]]
             answer += "L"
-        elif numbers[i] == 3 or numbers[i] == 6 or numbers[i] == 9:
-            right = numbers[i]
+        elif numbers[i] in right_key:
+            right = location[numbers[i]]
             answer += "R"
-        # 가운데 2,5,8,0 누를 경우 가까운 손가락 / 거리가 같으면 주손가락
-        # 거리를 어떻게 구할거냐? 그냥 빼는건 손가락이 가운데 있을 경우 오류 발생
-        else: 
-            if abs(left-numbers[i]) > abs(right-numbers[i]):
-                right = numbers[i]
-                answer += "R"
-            elif abs(left-numbers[i]) < abs(right-numbers[i]):
-                left = numbers[i]
-                answer += "L"                
-            if abs(left-numbers[i]) == abs(right-numbers[i]):
+        else:
+            l_distance = abs(left[0]-location[numbers[i]][0]) + 
+            abs(left[1]-location[numbers[i]][1])
+            r_distance = abs(right[0]-location[numbers[i]][0]) + 
+            abs(right[1]-location[numbers[i]][1])
+            
+            if l_distance < r_distance:
+                left = location[numbers[i]]
+                answer += "L"
+            elif l_distance > r_distance:
+                right = location[numbers[i]]
+                answer += "R"                
+            else:
                 if hand == "left":
-                    left = numbers[i]
-                    answer += "L"            
+                    left = location[numbers[i]]
+                    answer += "L"
                 else:
-                    right = numbers[i]
-                    answer += "R"                    
+                    right = location[numbers[i]]
+                    answer += "R"
         
-    print(answer)
+
     
     
     return answer
