dbf1d98cea607ea0d41b8d51cc4e7aa49742ee33
ghenmaru <63959171+rbgksqkr@users.noreply.github.com>
2021-11-21 23:07:48 +0900

    정렬

---
+++ 2021_11/number_sort_3.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = [0 for _ in range(10001)]
+
+for _ in range(N):
+  arr[int(input())] += 1
+for i in range(len(arr)):
+  if arr[i] != 0:
+    for _ in range(arr[i]):
+      print(i)
