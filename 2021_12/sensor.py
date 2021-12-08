fb752f9db0ed05ae7c860624488fcabfb85071c3
ghenmaru <63959171+rbgksqkr@users.noreply.github.com>
2021-12-08 23:26:19 +0900

    greedy

--- 2021_12/sensor.py
+++ 2021_12/sensor.py
@@ -3,7 +3,7 @@ input = sys.stdin.readline
 N = int(input())
 K = int(input())
 sensors = list(map(int, input().split()))
-betweenNum = []
+gap = []
 
 sensors.sort()
 
@@ -11,10 +11,10 @@ if N < K:
   print(0)
 else:
   for i in range(1, N):
-    betweenNum.append(sensors[i] - sensors[i-1])
-  betweenNum.sort()
+    gap.append(sensors[i] - sensors[i-1])
+  gap.sort()
 
   for _ in range(K-1):
-    betweenNum.pop()
+    gap.pop()
 
-  print(sum(betweenNum))
+  print(sum(gap))
