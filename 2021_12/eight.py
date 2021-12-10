6ea3e0bb005b0f4002d1ebd73c7b5ef0ff36852f
ghenmaru <63959171+rbgksqkr@users.noreply.github.com>
2021-12-10 22:51:26 +0900

    greedy

--- 2021_12/eight.py
+++ 2021_12/eight.py
@@ -3,12 +3,19 @@ input = sys.stdin.readline
 L, R = map(str, input().split())
 
 gap = len(R) - len(L)
-
+count = 0
 if gap == 0:
-  count = 0
-  for i in range(len(L)):
-    if L[i] == R[i] and L[i] == "8":
+  if L[0] != R[0]:
+    print(0)
+  else:
+    if L[0] == "8":
       count += 1
-  print(count)
+
+    for i in range(1, len(L)):
+      if L[i] != R[i]:
+        break
+      elif L[i] == R[i] and L[i] == "8":
+        count += 1
+    print(count)
 else:
   print(0)
