ca44b46be5d1415b53515632bff0543ebf71798e
ghenmaru <63959171+rbgksqkr@users.noreply.github.com>
2022-01-01 23:37:28 +0900

    DP

--- 2022_01/up_stairs.py
+++ 2022_01/up_stairs.py
@@ -11,7 +11,6 @@ d[1] = max(scores[0]+scores[1], scores[1])
 if N > 1:
   d[2] = max(d[1]+scores[2], scores[0]+scores[2])
   for i in range(3, N+1):
-    # 한 계단 vs 두 계단
     d[i] = max(d[i-3]+scores[i-1]+scores[i], d[i-2]+scores[i])
 
 print(d[-1])
