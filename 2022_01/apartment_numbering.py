f9311746f14f06ae72d7f564a5ae92d2ade50337
ghenmaru <63959171+rbgksqkr@users.noreply.github.com>
2022-01-04 22:58:21 +0900

    DFS

--- 2022_01/apartment_numbering.py
+++ 2022_01/apartment_numbering.py
@@ -5,47 +5,34 @@ input = sys.stdin.readline
 N = int(input())
 graph = []
 for _ in range(N):
-  graph.append(list(map(int, input().strip())))
+    graph.append(list(map(int, input().strip())))
 
+visited = [[0] * N] * N
+count = 0
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-
-
-visited = [[0]*N]*N
-print(graph)
-print(visited)
 def dfs(x, y):
-  count = 0
-  x, y = 0, 0
-  moves = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-  dx, dy = x, y
-  while True:
-    for i in range(4):
-      dx = x + moves[i][0]
-      dy = y + moves[i][1]
+    global count
 
-      if dx < 0 or dy < 0 or dx >= N or dy >= N: # 길 아님
-        continue
-      
-      if graph[dy][dx] == 1 and visited[dy][dx] == 0: # 집인데 안가봄
-        visited[dy][dx] = 1
+    if x < 0 or y < 0 or x >= N or y >= N:
+        return False
+
+    if graph[x][y] == 1:
         count += 1
-        x, y = dx, dy
-        break
-
-      elif graph[dy][dx] == 0 and visited[dy][dx] == 0: # 집 없는데 안가봄
-        graph[dy][dx] = 1
-        visited[dy][dx] = 1
-        x, y = dx, dy
-        break
-      
-      print(dx, dy)
-    if visited[x][y] == 0:
-      break
-
-
-  return count
+        graph[x][y] = 0
+        for i in range(4):
+            dfs(x + dx[i], y + dy[i])
+        return True
 
 answer = []
 for i in range(N):
-  for j in range(N):
-      answer.append(dfs(i,j))
+    for j in range(N):
+        if dfs(i, j) == True:
+            answer.append(count)
+            count = 0
+
+print(len(answer))
+answer.sort()
+for i in answer:
+    print(i)
