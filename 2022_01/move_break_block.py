17cc3c9c9238122ff9ee57a00bcd5ba61678a230
ghenmaru <63959171+rbgksqkr@users.noreply.github.com>
2022-01-10 22:50:01 +0900

    BFS

--- 2022_01/move_break_block.py
+++ 2022_01/move_break_block.py
@@ -2,43 +2,39 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-def bfs(x, y):
-  queue = deque([[x, y]])
-  flag = 1
-  mx = [-1, 1, 0, 0]
-  my = [0, 0, -1, 1]
+def bfs():
+  queue = deque([[0, 0, 1]])
+  visited = [[[0]*2 for i in range(M)] for j in range(N)]
+  visited[0][0][1] = 1
   while queue:
-    x, y = queue.popleft()
+    x, y, w = queue.popleft() # w == 1 일 때 벽을 뚫을 수 있음
+
+    if x == N-1 and y == M-1:
+      return visited[x][y][w]
+
+    mx = [-1, 1, 0, 0]
+    my = [0, 0, -1, 1]
+
     for i in range(4):
       dx = x + mx[i]
       dy = y + my[i]
+
       if dx < 0 or dy < 0 or dx >= N or dy >= M:
         continue
-      if graph[dx][dy] == 0:
-        graph[dx][dy] = graph[x][y] + 1
-        queue.append([dx, dy])
-    
-    if not queue and flag == 1: # 벽 한번 뚫기
-      for i in range(4):
-        dx = x + mx[i]
-        dy = y + my[i]
-        if dx < 0 or dy < 0 or dx >= N or dy >= M:
-          continue
-        graph[dx][dy] = graph[x][y] + 1
-        queue.append([dx, dy])
-      flag = 0
-    
-    print(graph, queue)
+      
+      if graph[dx][dy] == 1 and w == 1: # 벽이 있고 뚫을 수 있는 경우
+        visited[dx][dy][0] = visited[x][y][1] + 1
+        queue.append([dx, dy, 0])
+      
+      elif graph[dx][dy] == 0 and visited[dx][dy][w] == 0: # 벽이 없고 안가본 경우(일반적인 진행)
+        visited[dx][dy][w] = visited[x][y][w] + 1
+        queue.append([dx, dy, w])
+  
+  return -1
 
 N, M = map(int, input().split())
 graph = []
-
 for _ in range(N):
   graph.append(list(map(int, input().strip())))
 
-bfs(0, 0)
-
-if graph[N-1][M-1]:
-  print(graph[N-1][M-1]+1)
-else:
-  print(-1)
+print(bfs())
