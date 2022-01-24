2f48088d0e620f7ce3b60e915054d73b2dce74c1
ghenmaru <63959171+rbgksqkr@users.noreply.github.com>
2022-01-24 23:33:05 +0900

    topology_sort

--- 2022_01/line_up.py
+++ 2022_01/line_up.py
@@ -1,32 +1,36 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
 V, E = map(int, input().split())
 graph = [[] for _ in range(V+1)]
+indegree = [0] * (V+1)
 
 for i in range(E):
   a, b = map(int, input().split())
   graph[a].append(b) # a -> b
+  indegree[b] += 1 # b의 진입차수 증가
+
 
 def topology_sort():
-    N = len(graph)
-    stack = [] 
-    visited = [0 for _ in range(N)] 
-    for i in range(1, N):
-      if visited[i] == 0:
-          dfs(i, stack, visited)
-    
-    result = []
-    while len(stack) != 0:
-        result.append(stack.pop())
-    for i in result:
-      print(i, end=' ')
+  result = []
+  queue = deque()
+  # 진입차수가 0인 노드 찾기(그래프의 맨처음 찾기)
+  for i in range(1, V+1):
+    if indegree[i] == 0:
+      queue.append(i)
+  
+  while queue:
+    cur = queue.popleft()
+    result.append(cur)
+    # 해당 원소와 연결된 노드들의 진입차수에서 1빼기
+    for i in graph[cur]:
+      indegree[i] -= 1
+      if indegree[i] == 0:
+        queue.append(i)
+  
 
-def dfs(v, stack, visited):
-    visited[v] = 1
-    for i in graph[v]:
-        if visited[i] == 0: 
-          dfs(i, stack, visited)
-    stack.append(v)
+  for i in result:
+    print(i, end=' ')
 
 topology_sort()
