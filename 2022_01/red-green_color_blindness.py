b65a0cf0636dbda1059cccfc6617b58e90e3c18e
ghenmaru <63959171+rbgksqkr@users.noreply.github.com>
2022-01-26 23:32:24 +0900

    BFS

--- 2022_01/red-green_color_blindness.py
+++ 2022_01/red-green_color_blindness.py
@@ -4,16 +4,12 @@ input = sys.stdin.readline
 
 N = int(input())
 graph = []
+visited = [[0 for _ in range(N)] for _ in range(N)]
 for _ in range(N):
   graph.append(list(input().strip()))
 
 
-visited = [[0 for _ in range(N)] for _ in range(N)]
-
-def bfs(x, y, flag):
-  if visited[x][y] == 1:
-    return False
-  
+def bfs(x, y):
   queue = deque([[x, y]])
   mx = [-1, 1, 0, 0]
   my = [0, 0, -1, 1]
@@ -21,36 +17,37 @@ def bfs(x, y, flag):
 
   while queue:
     x, y = queue.popleft()
-    visited[x][y] = 1
-    for i in range(4):
-      dx = x + mx[i]
-      dy = y + my[i]
-      
-      if dx < 0 or dy < 0 or dx >= N or dy >= N:
-        continue    
-
-      if flag == 1 and graph[dx][dy] == 'G':
-        graph[dx][dy] = 'R'
-          
-      if graph[x][y] == graph[dx][dy] == color and visited[dx][dy] == 0:
-        queue.append([dx, dy])
-        visited[x][y] = 1
-
-  return True
-
+    if visited[x][y] == 0:
+      visited[x][y] = 1
+      for i in range(4):
+        dx = x + mx[i]
+        dy = y + my[i]
+        
+        if dx < 0 or dy < 0 or dx >= N or dy >= N:
+          continue    
+            
+        if graph[dx][dy] == color:
+          queue.append([dx, dy])
 
 cnt_1, cnt_2 = 0, 0
 flag = 0
 for i in range(N):
   for j in range(N):
-    if bfs(i, j, flag):
+    if visited[i][j] == 0: 
+      bfs(i, j)
       cnt_1 += 1
+
+for i in range(N):
+  for j in range(N):
+    if graph[i][j] == 'G':
+      graph[i][j] = 'R'
 
 visited = [[0 for _ in range(N)] for _ in range(N)]
 flag = 1
 for i in range(N):
   for j in range(N):
-    if bfs(i, j, flag):
+    if visited[i][j] == 0:
+      bfs(i, j)
       cnt_2 += 1
 
 print(cnt_1, cnt_2)
