92ce461dd936d091b6ebdac803adc2b050d3f6e9
ghenmaru <63959171+rbgksqkr@users.noreply.github.com>
2022-01-26 23:15:30 +0900

    BFS(시간초과)

--- 2022_01/red-green_color_blindness.py
+++ 2022_01/red-green_color_blindness.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
 N = int(input())
@@ -6,44 +7,50 @@ graph = []
 for _ in range(N):
   graph.append(list(input().strip()))
 
+
 visited = [[0 for _ in range(N)] for _ in range(N)]
 
-def dfs(x, y, flag):
-
+def bfs(x, y, flag):
+  if visited[x][y] == 1:
+    return False
+  
+  queue = deque([[x, y]])
   mx = [-1, 1, 0, 0]
   my = [0, 0, -1, 1]
+  color = graph[x][y]
 
-  if x < 0 or y < 0 or x >= N or y >= N:
-    return False
-
-  if graph[x][y] == 'R' and flag == 1:
+  while queue:
+    x, y = queue.popleft()
     visited[x][y] = 1
-    graph[x][y] = 0
     for i in range(4):
-      dfs(x + mx[i], y + my[i], flag)
-    return True
+      dx = x + mx[i]
+      dy = y + my[i]
+      
+      if dx < 0 or dy < 0 or dx >= N or dy >= N:
+        continue    
 
-  if graph[x][y] == 'G' and flag == 2:
-    visited[x][y] = 1
-    graph[x][y] = 0
-    for i in range(4):
-      dfs(x + mx[i], y + my[i], flag)
-    return True
+      if flag == 1 and graph[dx][dy] == 'G':
+        graph[dx][dy] = 'R'
+          
+      if graph[x][y] == graph[dx][dy] == color and visited[dx][dy] == 0:
+        queue.append([dx, dy])
+        visited[x][y] = 1
 
-  if graph[x][y] == 'B' and flag == 3:
-    visited[x][y] = 1
-    graph[x][y] = 0
-    for i in range(4):
-      dfs(x + mx[i], y + my[i], flag)
-    return True
+  return True
 
-count = 0  
+
+cnt_1, cnt_2 = 0, 0
+flag = 0
+for i in range(N):
+  for j in range(N):
+    if bfs(i, j, flag):
+      cnt_1 += 1
+
+visited = [[0 for _ in range(N)] for _ in range(N)]
 flag = 1
-for k in range(3):
-  for i in range(N):
-    for j in range(N):
-      if dfs(i, j, flag):
-        count += 1
-  flag += 1
+for i in range(N):
+  for j in range(N):
+    if bfs(i, j, flag):
+      cnt_2 += 1
 
-print(count)
+print(cnt_1, cnt_2)
