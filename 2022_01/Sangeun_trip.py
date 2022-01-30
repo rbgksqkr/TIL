4b2467ceb88ba7fe826e1842d9c66e6d2b458ad1
ghenmaru <63959171+rbgksqkr@users.noreply.github.com>
2022-01-30 22:39:05 +0900

    그래프 탐색

--- 2022_01/Sangeun_trip.py
+++ 2022_01/Sangeun_trip.py
@@ -4,27 +4,6 @@ input = sys.stdin.readline
 T = int(input())
 for _ in range(T):
   N, M = map(int, input().split())
-  graph = {i:[] for i in range(1, N+1)}
-  visited = [0 for _ in range(N+1)]
   for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-
-  count = 0
-  v = 0
-  for i in range(1, N+1):
-    if graph[i]:
-      v = i
-      break
-  
-  stack = []
-  visited[v] = 1
-  stack += graph[v]
-  while stack:
-    n = stack.pop()
-    if not visited[n]:
-      visited[n] = 1
-      count += 1
-      stack += graph[n]
-  print(count)
+    input()
+  print(N-1)
