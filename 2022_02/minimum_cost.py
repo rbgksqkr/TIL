549bbdb39cbc31ac8d1af3629d82445ae8ed301d
ghenmaru <63959171+rbgksqkr@users.noreply.github.com>
2022-02-06 22:28:13 +0900

    dijkstra

---
+++ 2022_02/minimum_cost.py
@@ -0,0 +1,35 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+N = int(input())
+M = int(input())
+INF = int(1e9)
+graph = [[] for _ in range(N+1)]
+distance = [INF] * (N+1)
+for _ in range(M):
+  a, b, cost = map(int, input().split())
+  graph[a].append((b, cost))
+start, end = map(int, input().split())
+
+
+def dijkstra(start):
+  queue = []
+  heapq.heappush(queue, (0, start))
+  distance[start] = 0
+  while queue:
+    dist, cur = heapq.heappop(queue)
+
+    if distance[cur] < dist:
+      continue
+    
+    for i in graph[cur]:
+      cost = dist + i[1]
+
+      if cost < distance[i[0]]:
+        distance[i[0]] = cost
+        heapq.heappush(queue, (cost, i[0]))
+
+
+dijkstra(start)
+print(distance[end])
