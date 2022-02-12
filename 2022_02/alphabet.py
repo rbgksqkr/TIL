d7d904ebf1cc25da483fea158177c113eb440a92
ghenmaru <63959171+rbgksqkr@users.noreply.github.com>
2022-02-12 22:17:26 +0900

    BFS

--- 2022_02/alphabet.py
+++ 2022_02/alphabet.py
@@ -1,43 +1,37 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 R, C = map(int, input().split())
-board = []
+graph = []
 for _ in range(R):
-  board.append(input().strip())
-
-graph = [[1 for _ in range(C)] for _ in range(R)]  
-visited = [[0 for _ in range(C)] for _ in range(R)]
-mx = [-1, 1, 0, 0]
-my = [0, 0, -1, 1]
+  graph.append(input().strip())
 
 
-def bfs(x, y):
-  queue = deque([[x,y,board[x][y]]])
-  visited[x][y] = 1
-  answer = ""
+def bfs():
+  count = 0
+  queue = set()
+  queue.add((0, 0, graph[0][0]))
   while queue:
-    x, y, char = queue.popleft()
-    # if char in answer:
-    #   continue
-
-    if board[x][y] not in answer:
-      answer += char
+    x, y, alpha = queue.pop()
+    mx = [-1, 1, 0, 0]
+    my = [0, 0, -1, 1]
+    end = alpha
     for i in range(4):
       dx = x + mx[i]
       dy = y + my[i]
 
       if dx < 0 or dy < 0 or dx >= R or dy >= C:
+        continue   
+      if graph[dx][dy] in alpha:
         continue
-
-      if visited[dx][dy] == 0:
-        visited[dx][dy] = 1
-        queue.append([dx, dy, board[dx][dy]])
-
-        # graph[dx][dy] = max(graph[dx][dy], graph[x][y] + 1)
-
-      print(queue, answer)
-
+      else:
+        end = alpha+graph[dx][dy]
+        queue.add((dx, dy, end))
+    
+    if alpha == end:
+      count = max(count, len(alpha))
   
-bfs(0, 0)
+  return count
+
+
+print(bfs())
