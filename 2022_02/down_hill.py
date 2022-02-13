31c9ccf56ae92f20222c946ad18ccf9637fd4952
ghenmaru <63959171+rbgksqkr@users.noreply.github.com>
2022-02-13 23:07:28 +0900

    DFS & DP

--- 2022_02/down_hill.py
+++ 2022_02/down_hill.py
@@ -1,36 +1,28 @@
 import sys
 input = sys.stdin.readline
-
-M, N = map(int, input().split())
-
-graph = []
-
-for _ in range(M):
-  graph.append(list(map(int, input().split())))
+sys.setrecursionlimit(100000)
+m, n = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(m)]
+dp = [[-1] * n for _ in range(m)] # 내리막길 경우의 수
 
 mx = [-1, 1, 0, 0]
 my = [0, 0, -1, 1]
 
 
-def dfs():
-  stack = [[0, 0]]
-  count = 0
-  while stack:
-    x, y = stack.pop()
-    if graph[x][y] == graph[M-1][N-1]:
-      count += 1
-      continue
-    for i in range(4):
-      dx = x + mx[i]
-      dy = y + my[i]
+def dfs(x, y):
+  if x == m-1 and y == n-1: # 목적지까지 가면 경로가 있으니까 +1
+      return 1
 
-      if dx < 0 or dy < 0 or dx >= M or dy >= N:
-        continue
-  
-      if graph[x][y] > graph[dx][dy]:
-        stack.append([dx, dy])
+  if dp[x][y] == -1: # 안가봤으면
+      dp[x][y] = 0 # 목적지까지 못간다고 가정하고
+      for i in range(4):
+        dx = x + mx[i]
+        dy = y + my[i]
+        if 0 <= dx < m and 0 <= dy < n:
+            if graph[dx][dy] < graph[x][y]: # 내리막길이면
+                dp[x][y] += dfs(dx, dy) # 재귀
 
-  return count
+  return dp[x][y] # 이전의 연산한 방문경로가 있으므로 그 값 반환
 
 
-print(dfs())
+print(dfs(0, 0))
