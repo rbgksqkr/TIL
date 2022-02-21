46aec8d57e6dfc55340d1035df2728816113189a
ghenmaru <63959171+rbgksqkr@users.noreply.github.com>
2022-02-21 22:16:14 +0900

    Floyd Warshall

--- 2022_01/path_finding.py
+++ 2022_01/path_finding.py
@@ -2,34 +2,24 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-
-graph = {i:[] for i in range(N)}
-visited = [0 for _ in range(N)]
-arr = [[0 for _ in range(N)] for _ in range(N)]
-
+INF = int(1e9)
+distance = [[INF for _ in range(N)] for _ in range(N)] # 거리 무한으로 초기화
 for i in range(N):
   temp = list(map(int, input().split()))
   for j in range(len(temp)):
     if temp[j] == 1:
-      graph[i].append(j)
+      distance[i][j] = 1 # 이어지는 정점 간의 거리 존재 유무
 
-
-def dfs(n):
-  stack = [n]
-
-  while stack:
-    n = stack.pop()
-
-    if not visited[n]:
-
-      for i in graph[n]:
-        arr[n][i] = 1
-        arr[i][n] = 1
-      visited[n] = 1
-      stack += graph[n]
-
+for k in range(N):
+  for i in range(N):
+    for j in range(N):
+      if distance[i][j] > distance[i][k] + distance[k][j]: # 어떻게든 가는 길이 있으면
+        distance[i][j] = distance[i][k] + distance[k][j] # 무한보다 작기 때문에 값 바뀜.
 
 for i in range(N):
-  if graph[i]:
-    dfs(i)
-print(arr)
+  for j in range(N):
+    if distance[i][j] == INF:
+      print(0, end=' ')
+    else:
+      print(1, end=' ')
+  print()
