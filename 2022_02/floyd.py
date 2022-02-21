bf2f2e8d0654fea1f6760491768a496379f8d1ee
ghenmaru <63959171+rbgksqkr@users.noreply.github.com>
2022-02-21 22:41:32 +0900

    Floyd Warshall

---
+++ 2022_02/floyd.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+INF = int(1e9)
+distance = [[INF for _ in range(n+1)] for _ in range(n+1)]
+
+for i in range(1, n+1):
+  for j in range(1, n+1):
+    if i == j:
+      distance[i][j] = 0
+
+for _ in range(m):
+  a, b, c = map(int, input().split())
+  distance[a][b] = min(distance[a][b], c)
+
+for k in range(1, n+1):
+  for i in range(1, n+1):
+    for j in range(1, n+1):
+      if distance[i][j] > distance[i][k] + distance[k][j]:
+        distance[i][j] = distance[i][k] + distance[k][j]
+      
+for i in range(1, n+1):
+  for j in range(1, n+1):
+    if distance[i][j] == INF:
+      print(0, end=' ')
+    else:
+      print(distance[i][j], end=' ')
+  print()
