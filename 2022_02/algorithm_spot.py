24b20658f4fb8fd93a135bbbdb86a1d950ad0d36
ghenmaru <63959171+rbgksqkr@users.noreply.github.com>
2022-02-21 23:28:50 +0900

    BFS

--- 2022_02/algorithm_spot.py
+++ 2022_02/algorithm_spot.py
@@ -3,35 +3,35 @@ from collections import deque
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
+visited = [[-1 for _ in range(N)] for _ in range(M)]
 graph = []
-visited = [[0 for _ in range(N)] for _ in range(M)]
 for _ in range(M):
   graph.append(list(map(int, input().strip())))
 
-mx = [-1, 1, 0, 0]
-my = [0, 0, -1, 1]
 
-def bfs():
-  queue = deque([[0, 0]])
+def bfs(x, y):
+  queue = deque()
+  queue.append([x, y])
+  visited[x][y] = 0
+  mx = [-1, 1, 0, 0]
+  my = [0, 0, -1, 1]
+
   while queue:
-    x, y = queue.popleft()
-
-    if x == M-1 and y == N-1:
-      return True
-
-    
+    x, y  = queue.popleft()
     for i in range(4):
       dx = x + mx[i]
       dy = y + my[i]
-
       if dx < 0 or dy < 0 or dx >= M or dy >= N:
         continue
-      
-      elif visited[dx][dy] == 0:
-        visited[dx][dy] = 1
-        if graph[dx][dy] == 1:
-          graph[dx][dy] = graph[x][y] + 1
-        queue.append([dx, dy])
-      
-bfs()
-print(max(graph[N-1][M-2], graph[N-2][M-2]))
+      if visited[dx][dy] == -1:
+        if graph[dx][dy] == 0:
+          queue.appendleft([dx, dy])
+          visited[dx][dy] = visited[x][y]
+        elif graph[dx][dy] == 1:
+          queue.append([dx, dy])
+          visited[dx][dy] = visited[x][y] + 1
+  
+  print(visited[M-1][N-1])
+
+
+bfs(0, 0)
