4500d19f3a2a6bd9828e7ae5e98b2cf68065f1ef
ghenmaru <63959171+rbgksqkr@users.noreply.github.com>
2022-02-25 23:10:47 +0900

    DP

--- 2022_02/move_pipe.py
+++ 2022_02/move_pipe.py
@@ -3,30 +3,26 @@ input = sys.stdin.readline
 
 N = int(input())
 graph = []
-dp = [[-1 for _ in range(N)] for _ in range(N)]
 for _ in range(N):
   graph.append(list(map(int, input().split())))
+result = [[[0 for _ in range(N)] for _ in range(N)] for _ in range(3)]
+result[0][0][1] = 1 
+# -----------------------------------------------------------
+# 첫번째 행 : 가로(0), 세로(1), 대각선(2)으로 갈 수 있는 경우의 수
+# 두번째 행, 세번째 행 : 좌표
+# result[0][0][1] = 1 : 가로로 (0,1)을 갈 수 있는 경우의 수 : 1
+# -----------------------------------------------------------
+for i in range(2, N): # 가로로 시작해서 첫번째줄의 가로 dp테이블 초기화
+  if graph[0][i] == 0:
+    result[0][0][i] = result[0][0][i-1]
 
-degree = 0 # 가로 : 0, 세로 : 1, 대각선 : 2
-answer = 0
-def dfs(x, y, degree):
-  global answer
-  if x == N-1 and y == N-1: # 목적지에 도착하면 +1
-    answer += 1
-    return
+for i in range(1, N):
+  for j in range(2, N):
+    if graph[i][j] == 0 and graph[i-1][j] == 0 and graph[i][j-1] == 0: # 앞, 아래, 대각선에 길이 있을 때
+      #가로+대각선, 세로+대각선, 대각선+대각선
+      result[2][i][j] = result[0][i-1][j-1] + result[1][i-1][j-1] + result[2][i-1][j-1]  
+    if graph[i][j] == 0:
+      result[0][i][j] = result[0][i][j-1] + result[2][i][j-1] # 가로+가로, 대각선+가로
+      result[1][i][j] = result[1][i-1][j] + result[2][i-1][j] # 세로+세로, 대각선+세로
 
-  if degree == 0 or degree == 2: # 앞으로 전진하는 경우
-    if y+1 < N:
-      if graph[x][y+1] == 0:
-        dfs(x, y+1, 0) # 전진하면 모양은 가로가 되므로 degree = 0
-  if degree == 1 or degree == 2:  # 아래로 내려가는 경우
-    if x+1 < N:
-      if graph[x+1][y] == 0:
-        dfs(x+1, y, 1) # 내려가면 모양은 세로가 되므로 degree = 1
-  if degree == 0 or degree == 1 or degree == 2: # 대각선으로 내려가는 경우
-    if x+1 < N and y+1 < N:
-      if graph[x+1][y] == 0 and graph[x][y+1] == 0 and graph[x+1][y+1] == 0:
-        dfs(x+1, y+1, 2) # 대각선 모양 유지 degree = 2
-
-dfs(0, 1, 0)
-print(answer)
+print(result[0][N-1][N-1] + result[1][N-1][N-1] + result[2][N-1][N-1]) # 가로, 세로, 대각선 경우의 수의 합
