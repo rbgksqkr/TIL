c149612f169999fd9a40049cac6105f682f39208
ghenmaru <63959171+rbgksqkr@users.noreply.github.com>
2022-10-12 11:59:36 +0900

    BFS

--- 이코테/3. 탐색/Q16_연구소.py
+++ 이코테/3. 탐색/Q16_연구소.py
@@ -1,58 +1,63 @@
 from collections import deque
 import copy
+from itertools import combinations
+n, m = map(int, input().split())
 
-# 벽 만들기
-# 빈 칸을 벽으로 만들고 count+1을 넣어 재귀적으로 호출한다.
-# 벽을 3개를 세웠을 때 bfs()를 실행한다.
-def make_wall(count):
-    if count == 3:
-        bfs()
-        return
+maps = []
+virus = []
+for i in range(n):
+    maps.append(list(map(int, input().split())))
+    for j in range(m):
+        if maps[i][j] == 2:
+            virus.append((i, j))
+
+
+def makeWall():
+    spaces = []
     for i in range(n):
         for j in range(m):
-            if labs[i][j] == 0:
-                labs[i][j] = 1
-                make_wall(count+1)
-                labs[i][j] = 0
+            if maps[i][j] == 0:
+                spaces.append((i, j))
 
-# 바이러스 퍼뜨리고 안전지역 계산하기
-# 벽을 어떻게 세우냐에 따라 바이러스 퍼지는 게 다르기 때문에 그값을 저장하기 위해 임시 리스트를 만든다.
-# 바이러스를 퍼뜨린 후 안전 지역을 계산한다.
-def bfs():
-    queue = deque()
-    test_labs = copy.deepcopy(labs)
+    max_count = 0
+    for space in list(combinations(spaces, 3)):
+        temp_map = copy.deepcopy(maps)
+        for x, y in space:
+            temp_map[x][y] = 1
+        spread(temp_map)
+        max_count = max(max_count, getScore(temp_map))
+        for x, y in space:
+            temp_map[x][y] = 0
+    print(max_count)
+
+
+def spread(temp_map):
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
-    for i in range(n):
-        for j in range(m):
-            if labs[i][j] == 2:
-                queue.append([i, j])  
+
+    queue = deque(virus)
     while queue:
         x, y = queue.popleft()
+
         for i in range(4):
             mx = x + dx[i]
             my = y + dy[i]
+
             if mx < 0 or my < 0 or mx >= n or my >= m:
                 continue
-            if test_labs[mx][my] == 0:
-                test_labs[mx][my] = 2
-                queue.append([mx, my])
 
-    global answer
+            if temp_map[mx][my] == 0:
+                temp_map[mx][my] = 2
+                queue.append((mx, my))
+
+
+def getScore(temp_map):
     count = 0
     for i in range(n):
         for j in range(m):
-            if test_labs[i][j] == 0:
-                count += 1 
-    answer = max(answer, count)
+            if temp_map[i][j] == 0:
+                count += 1
+    return count
 
 
-
-n, m = map(int, input().split())
-labs = []
-for i in range(n):
-    labs.append(list(map(int, input().split())))
-
-answer = 0
-make_wall(0)
-print(answer)
+makeWall()
