5ae77f9a18b021559833a2d5cd8a00e666eaef4e
ghenmaru <63959171+rbgksqkr@users.noreply.github.com>
2022-10-19 12:20:16 +0900

    greedy

--- 이코테/1. 그리디/Q4_만들수없는금액.py
+++ 이코테/1. 그리디/Q4_만들수없는금액.py
@@ -1,36 +1,16 @@
-# from itertools import combinations
-# n = int(input())
-
-# prices = list(map(int, input().split()))
-
-
-# a = []
-# result = []
-# for i in range(n):
-#     a += list(combinations(prices, i))
-
-# for i in range(len(a)):
-#     result.append(sum(a[i]))
-
-# result.sort()
-# result = list(set(result))
-
-# for i in range(len(result)):
-#     if result[i] != i:
-#         print(i)
-#         break
-
 n = int(input())
-prices = list(map(int, input().split()))
+coins = list(map(int, input().split()))
+coins.sort()
 
-prices.sort()
-print(prices)
+answer = coins[0]
 
-target = 1 # 1 ~ (target - 1) 까지의 금액을 모두 만들 수 있다
-for price in prices:
-    if target < price:
+
+for i in range(1, n):
+    if answer + 1 < coins[i]:
+        answer = answer + 1
         break
-
-    target += price
-
-print(target)
+    if i == n-1:
+        answer += coins[i] + 1
+    else:
+        answer += coins[i]
+print(answer)
