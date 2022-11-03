580ec7a619aa317077d4d2fc20e1c26c16a54d2e
ghenmaru <63959171+rbgksqkr@users.noreply.github.com>
2022-11-04 00:02:53 +0900

    구현

--- 코테JS/치킨쿠폰.js
+++ 코테JS/치킨쿠폰.js
@@ -2,11 +2,9 @@ function solution(chicken) {
     let answer = 0;
     let coupon = chicken;
     const ORDER_CHICKEN = 10;
-    while (coupon > 1){
-        coupon = chicken / ORDER_CHICKEN;
-        answer += coupon
-        chicken = coupon        
+    while (coupon >= 10){
+        answer += parseInt(coupon / ORDER_CHICKEN);
+        coupon = parseInt(coupon / ORDER_CHICKEN) + coupon % ORDER_CHICKEN;
     }
-
     return parseInt(answer);
 }
