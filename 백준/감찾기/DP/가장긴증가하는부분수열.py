461d82d3d8efa6703c3a4eac5b15d107b1ef1758
ghenmaru <63959171+rbgksqkr@users.noreply.github.com>
2023-03-21 13:45:26 +0900

    DP

---
+++ 백준/감찾기/DP/가장긴증가하는부분수열.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+numbers = list(map(int, input().split()))
+dp = [1 for _ in range(1001)]
+
+for i in range(n):
+    for j in range(i):
+        if numbers[i] > numbers[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+print(max(dp))
