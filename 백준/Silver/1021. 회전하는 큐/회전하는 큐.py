fd2d9fa58a1cf45561406b5181dbda4e050b49a1
Gyu <63959171+rbgksqkr@users.noreply.github.com>
2023-06-23 16:46:24 +0900

    [Silver III] Title: 회전하는 큐, Time: 64 ms, Memory: 34120 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
@@ -1,14 +1,11 @@
 import sys
 from collections import deque
-import copy
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
 targets = list(map(int, input().split()))
 queue = deque([i for i in range(1, N+1)])
-origin = copy.deepcopy(queue)
-
 count = 0
 for target in targets:
     index = queue.index(target)
@@ -17,7 +14,7 @@ for target in targets:
     if index == 0:
         queue.popleft()
         continue
-    if index <= n-index-1:
+    if index <= n // 2:
         dist = index
         queue.rotate(-dist)
     else:
