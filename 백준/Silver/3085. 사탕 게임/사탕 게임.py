44c6c7813e3b739193d4c4326277bd5c75d44bed
Gyu <63959171+rbgksqkr@users.noreply.github.com>
2023-07-05 14:05:25 +0900

    [Silver II] Title: 사탕 게임, Time: 4980 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,43 +1,62 @@
-def check(data):
-    max_cnt = 1
-    for i in range(N):
-        cnt = 1
-        for j in range(1, N):
-            if data[i][j] == data[i][j-1]:
-                cnt += 1
-            else:
-                cnt = 1
-            max_cnt = max(max_cnt, cnt)
-
-        cnt = 1
-        for j in range(1, N):
-            if data[j][i] == data[j-1][i]:
-                cnt += 1
-            else:
-                cnt = 1
-            max_cnt = max(max_cnt, cnt)
-    
-    return max_cnt
+import sys
+input = sys.stdin.readline
 
 N = int(input())
-data = [list(input()) for _ in range(N)]
-ans = 0
+graph = []
+for _ in range(N):
+    graph.append(list(input().strip()))
 
+def getResult(graph):
+    max_value = 1
+    for i in range(N):
+        result = 1
+        for j in range(1, N):
+            if graph[i][j] == graph[i][j-1]:
+                result += 1
+            else: 
+                result = 1
+            max_value = max(max_value, result)
+
+        result = 1
+        for j in range(1, N):
+            if graph[j][i] == graph[j-1][i]:
+                result += 1
+            else:
+                result = 1
+            max_value = max(max_value, result)
+    return max_value
+
+# def getResult(data):
+#     max_cnt = 1
+#     for i in range(N):
+#         cnt = 1
+#         for j in range(1, N):
+#             if data[i][j] == data[i][j-1]:
+#                 cnt += 1
+#             else:
+#                 cnt = 1
+#             max_cnt = max(max_cnt, cnt)
+
+#         cnt = 1
+#         for j in range(1, N):
+#             if data[j][i] == data[j-1][i]:
+#                 cnt += 1
+#             else:
+#                 cnt = 1
+#             max_cnt = max(max_cnt, cnt)
+    
+#     return max_cnt
+
+answer = 0
 for i in range(N):
     for j in range(N):
-        if j + 1 < N: # 열 범위 체크
-            # 인접한 것끼리 바꿔주기
-            data[i][j], data[i][j+1] = data[i][j+1], data[i][j]
-            cnt = check(data)
-            ans = max(ans, cnt)
-            # 바꾼 것 원래대로 돌려놓기
-            data[i][j], data[i][j+1] = data[i][j+1], data[i][j]
+        if j+1 < N:
+            graph[i][j], graph[i][j+1] = graph[i][j+1], graph[i][j]
+            answer = max(answer, getResult(graph))
+            graph[i][j], graph[i][j+1] = graph[i][j+1], graph[i][j]
+        if i+1 < N:
+            graph[i][j], graph[i+1][j] = graph[i+1][j], graph[i][j]
+            answer = max(answer, getResult(graph))
+            graph[i][j], graph[i+1][j] = graph[i+1][j], graph[i][j]
 
-        if i + 1 < N: # 행 범위 체크
-            data[i][j], data[i+1][j] = data[i+1][j], data[i][j]
-            cnt = check(data)
-            ans = max(ans, cnt)
-            # 바꾼 것 원래대로 돌려놓기
-            data[i][j], data[i+1][j] = data[i+1][j], data[i][j]
-
-print(ans)
+print(answer)
