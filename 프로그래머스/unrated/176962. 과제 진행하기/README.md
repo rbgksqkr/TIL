9b2f5c0ee2f49d5a914720d437ef7d88d9c33236
Gyu <63959171+rbgksqkr@users.noreply.github.com>
2023-09-02 16:26:24 +0900

    [unrated] Title: 과제 진행하기, Time: 3.14 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/unrated/176962. 과제 진행하기/README.md
+++ 프로그래머스/unrated/176962. 과제 진행하기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 과제 진행하기 - 176962 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/176962) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/176962#) 
 
 ### 성능 요약
 
-메모리: 10.6 MB, 시간: 1.99 ms
+메모리: 10.7 MB, 시간: 3.14 ms
 
 ### 구분
 
--- 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.py
@@ -12,12 +12,14 @@ def solution(plans):
         if diff_hour > 0: diff_minute += diff_hour * 60
         rest_time = int(plans[i-1][2]) - diff_minute
         
+        # 과제가 끝나기 전에 새로운 과제를 시작할 시각이 되었을 때
         if rest_time > 0:
             rest_assignment.append([plans[i-1][0],rest_time])
             
+        # 새로운 과제 시작 전 과제가 끝남
         if rest_time <= 0:
             answer.append(plans[i-1][0])
-            # plans 중간에 시간이 남을 때
+            # 다음 과제 시작 전 시간이 남을 때
             while len(rest_assignment) > 0:
                 rest_assignment[-1][1] -= abs(rest_time)
                 if rest_assignment[-1][1] <= 0: 
@@ -28,7 +30,7 @@ def solution(plans):
         
     answer.append(plans[-1][0])
     
-    # TODO: plans 끝나고 최근 목록부터 처리
+    # plans 끝나고 최근 목록부터 처리
     while len(rest_assignment) > 0:
         answer.append(rest_assignment.pop()[0])
     return answer
