28b763d4a5526032d36ffc8e4a26be60313e781d
Gyu <63959171+rbgksqkr@users.noreply.github.com>
2023-09-03 13:46:00 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.py
@@ -1,3 +1,4 @@
+# 위아래 커서 이동 최솟값
 def currentMinCount(target):
     start = ord('A')
     end = ord('Z') + 1
@@ -14,12 +15,17 @@ def solution(name):
     
     for i, target in enumerate(name):
         answer += currentMinCount(target)     
-        nextIndex = i + 1
         
+        # 해당 알파벳 다음부터 연속된 A 문자열 찾기
+        nextIndex = i + 1
         while nextIndex < len(name) and name[nextIndex] == 'A':
             nextIndex += 1
-            
-        minMove = min(minMove, i + i + len(name) - nextIndex, i+(len(name) - nextIndex)*2)
+        
+        # 왼쪽 오른쪽 커서 이동 최솟값
+        # 1. 모든 알파벳 지나가기
+        # 2. A가 가장 많이 연속되는 부분의 앞부분을 2번지나기
+        # 3. A가 가장 많이 연속되는 부분의 뒷부분을 2번지나기
+        minMove = min(minMove, i + i + len(name) - nextIndex, i + (len(name) - nextIndex)*2)
     answer += minMove
     
     return answer
