f3e5762389a82c8b4138b1795af9dabcab586b6c
Gyu <63959171+rbgksqkr@users.noreply.github.com>
2023-11-02 10:05:13 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 3656 ms, Memory: 36332 KB -BaekjoonHub

---
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -0,0 +1,153 @@
+import sys
+import math
+from collections import deque
+input = sys.stdin.readline
+
+R, C, T = map(int, input().split())
+
+vacc_up, vacc_down = [], []
+graph = []
+mise = []
+for i in range(R):
+  row = list(map(int, input().split()))
+  for j in range(C):
+    if row[j] > 0:
+      mise.append([i, j, 0])
+  graph.append(row)
+
+# 진공 청소기 위치 저장
+for i in range(R):
+  if graph[i][0] == -1:
+    if graph[i][0]:
+      vacc_up = [i, 0]
+      vacc_down = [i+1, 0]
+      break
+
+
+dx = [-1, 0, 1, 0] # 위부터 시계방향
+dy = [0, 1, 0, -1]
+
+
+def spread():
+  mise_graph = [[0 for _ in range(C)] for _ in range(R)]
+
+  for x in range(R):
+    for y in range(C):
+      if graph[x][y] == 0 or graph[x][y] == -1:
+        continue
+    
+      for i in range(4):
+        mx = x + dx[i]
+        my = y + dy[i]
+        if mx < 0 or my < 0 or mx >= R or my >= C:
+          continue
+
+        if graph[mx][my] == -1:
+          continue
+
+        mise_graph[mx][my] += graph[x][y] // 5
+        mise_graph[x][y] -= graph[x][y] // 5
+  for i in range(R):
+    for j in range(C):
+      graph[i][j] += mise_graph[i][j]
+
+
+def getSum(graph):
+  mise_sum = 0
+  row = len(graph)
+  col = len(graph[0])
+  for i in range(row):
+    for j in range(col):
+      if graph[i][j] != -1:
+        mise_sum += graph[i][j]
+  return mise_sum
+  
+
+# def moveVaccUp(coord):
+#   x, y, dir = coord[0], 1, 1
+#   while True:
+#     if x == coord[0] and y == 0:
+#       break
+
+#     mx = x + dx[dir]
+#     my = y + dy[dir]
+#     if mx < 0 or my < 0 or mx > coord[0] or my >= C:
+#       dir = (dir - 1) % 4
+#       continue
+
+#     graph[x][y], graph[mx][my] = graph[mx][my], graph[x][y]
+    
+#     # if graph[mx][my] and graph[mx][my] != -1:
+#     #   graph[x][y] = graph[mx][my]
+#     #   graph[mx][my] = 0
+#     x, y = mx, my
+
+# def moveVaccDown(coord):
+#   x, y, dir = coord[0], 1, 1
+  
+#   while True:
+#     if x == coord[0] and y == 0:
+#       break
+    
+#     mx = x + dx[dir]
+#     my = y + dy[dir]
+#     if mx < coord[0] or my < 0 or mx >= R or my >= C:
+#       dir = (dir + 1) % 4
+#       continue
+
+#     graph[x][y], graph[mx][my] = graph[mx][my], graph[x][y]
+#     x, y = mx, my
+
+
+
+# def printGraph(graph):
+#   print()
+#   row = len(graph)
+#   col = len(graph[0])
+#   for i in range(row):
+#     for j in range(col):
+#       print(graph[i][j], end=' ')
+#     print()
+
+
+def clean_top():
+    dx, dy = (0, -1, 0, 1), (1, 0, -1, 0)
+    x, y, d = vacc_up[0], 1, 0
+    prev = 0
+
+    while True:
+        mx, my = x + dx[d], y + dy[d]
+
+        if x == vacc_up[0] and y == 0:
+            break
+        if not 0 <= mx < R or not 0 <= my < C:
+            d += 1
+            continue
+
+        graph[x][y], prev = prev, graph[x][y]
+        x, y = mx, my
+
+
+def clean_bottom():
+    dx, dy = (0, 1, 0, -1), (1, 0, -1, 0)
+    x, y, d = vacc_down[0], 1, 0
+    prev = 0
+
+    while True:
+        mx, my = x + dx[d], y + dy[d]
+
+        if x == vacc_down[0] and y == 0:
+            break
+        if not 0 <= mx < R or not 0 <= my < C:
+            d += 1
+            continue
+
+        graph[x][y], prev = prev, graph[x][y]
+        x, y = mx, my
+
+for _ in range(T):
+  spread()
+  clean_top()
+  clean_bottom()
+
+print(getSum(graph))
