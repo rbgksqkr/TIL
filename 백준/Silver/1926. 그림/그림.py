ee2452020169bc2ca79876b4bbb3fd21c82008a3
Gyuhan Park <rbgks1937@naver.com>
2024-01-09 16:02:22 +0900

    [Silver I] Title: 그림, Time: 360 ms, Memory: 36928 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,51 +1,49 @@
-import sys
 from collections import deque
+import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-canvas = []
+board = []
 for _ in range(n):
-  canvas.append(list(map(int, input().split())))
-visited = [[0 for _ in range(m)] for _ in range(n)]
+    line = list(map(int, input().split()))
+    board.append(line)
+
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 
 def bfs(x, y):
-  queue = deque([[x,y]])
-  visited[x][y] = 1
-  count = 1
-  mx = [-1, 1, 0, 0]
-  my = [0, 0, -1, 1]
+    queue = deque()
+    queue.append([x, y])
+    visited[x][y] = 1
+    count = 1
+    while queue:
+        x, y = queue.popleft()
 
-  while queue:
-    x, y = queue.popleft()
-    for i in range(4):
-      dx = x + mx[i]
-      dy = y + my[i]
+        for i in range(4):
+            mx = x + dx[i]
+            my = y + dy[i]
 
-      if dx < 0 or dy < 0 or dx >= n or dy >= m:
-        continue
-      
-      elif canvas[dx][dy] == 1 and visited[dx][dy] == 0:
-        visited[dx][dy] = 1
-        count += 1
-        queue.append([dx,dy])
-  
-  return count
-        
+            if mx < 0 or my < 0 or mx >= n or my >= m:
+                continue
 
-total = 0
-max_count = 0
+            if board[mx][my] and not visited[mx][my]:
+                visited[mx][my] = 1
+                count += 1
+                queue.append([mx, my])
+    return count
+
+
+result = []
+visited = [[0 for _ in range(m)] for _ in range(n)]
 for i in range(n):
-  for j in range(m):
-    if visited[i][j] == 0 and canvas[i][j] == 1:
-        max_count = max(bfs(i, j), max_count)
-        count = 0
-        total += 1
+    for j in range(m):
+        if board[i][j] and not visited[i][j]:
+            result.append(bfs(i, j))
 
-print(total)
-if total:
-  print(max_count)
+if result:
+    print(len(result))
+    print(max(result))
 else:
-  print(0)
+    print(0)
+    print(0)
