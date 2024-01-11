dc67b5a456a26194d83689db0178d2e721131784
Gyuhan Park <rbgks1937@naver.com>
2024-01-12 02:51:11 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 872 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+
+def func(a, b, n):
+    if n == 1:
+        print(a, b)
+        return
+    func(a, 6-a-b, n-1)
+    print(a, b)
+    func(6-a-b, b, n-1)
+
+
+k = int(input())
+print(2**k-1)
+func(1, 3, k)
