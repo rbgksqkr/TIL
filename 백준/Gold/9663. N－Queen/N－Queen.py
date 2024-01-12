aa7fcb489b27ee51fcee81dc8b68295edb07a231
Gyuhan Park <rbgks1937@naver.com>
2024-01-13 00:39:59 +0900

    [Gold IV] Title: N-Queen, Time: 25696 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -3,34 +3,31 @@ input = sys.stdin.readline
 
 n = int(input())
 
-answer = 0
-row = [0 for _ in range(n)]  # 1행에 1개밖에 못놓으므로 row 1차원 배열로 선언.
-board = [[0 for _ in range(n)] for _ in range(n)]
+board = []
+count = 0
+
+isUsed1 = [0 for _ in range(40)]
+isUsed2 = [0 for _ in range(40)]
+isUsed3 = [0 for _ in range(40)]
 
 
-def check(x):
-    for i in range(x):
-        # 같은 열인지 확인.
-        # 대각선 체크.
-        # 맨 윗 행부터 채우므로 x보다 작은 값만 체크.
-        # x와 i의 세로길이와 가로길이가 같으면 대각선에 위치한 것으로 체크.
-        if row[x] == row[i] or abs(row[x] - row[i]) == x - i:
-            return False
-    return True
-
-
-def queen(x):
-    global answer
-    if x == n:  # 탈출 조건. 퀸 n개를 보드판에 모두 배치.
-        answer += 1
+def func(cur):
+    global count
+    if cur == n:
+        count += 1
         return
 
-    # (x, i)에 퀸을 놓겠다
     for i in range(n):
-        row[x] = i  # 일단 배치하고
-        if check(x):  # promising 한 위치에
-            queen(x+1)  # 다음 퀸 배치
+        if isUsed1[i] or isUsed2[cur+i] or isUsed3[cur-i+n-1]:
+            continue
+        isUsed1[i] = 1
+        isUsed2[cur+i] = 1
+        isUsed3[cur-i+n-1] = 1
+        func(cur+1)
+        isUsed1[i] = 0
+        isUsed2[cur+i] = 0
+        isUsed3[cur-i+n-1] = 0
 
 
-queen(0)
-print(answer)
+func(0)
+print(count)
