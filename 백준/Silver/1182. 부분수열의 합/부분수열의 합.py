994e5486d0804396600120407e05612b113a8f8b
Gyuhan Park <rbgks1937@naver.com>
2024-01-14 23:54:06 +0900

    [Silver II] Title: 부분수열의 합, Time: 240 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,30 +1,24 @@
 import sys
-
 input = sys.stdin.readline
 
-N, S = map(int, input().split())
+
+n, s = map(int, input().split())
 arr = list(map(int, input().split()))
 count = 0
 
 
 def func(cur, total):
     global count
-    if cur == N:
-        if total == S:
+    if cur == n:
+        if total == s:
             count += 1
         return
-    func(cur+1, total)
-    func(cur+1, total+arr[cur])
-    # for i in range(N):
-    #     if isUsed[i]:
-    #         continue
-    #     print('i, cur, count:', i, cur, count)
 
-    #     isUsed[i] = 1
-    #     print('arr[i]:', cur+arr[i])
-    #     func(cur+arr[i])
-    #     isUsed[i] = 0
+    func(cur+1, total)
+    func(cur+1, total + arr[cur])
+
+
 func(0, 0)
-if S == 0:
+if s == 0:
     count -= 1
 print(count)
