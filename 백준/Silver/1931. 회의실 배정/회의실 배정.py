85818cda0750327c5e01249dfdc8f861c3985c03
Gyuhan Park <rbgks1937@naver.com>
2024-01-22 13:20:18 +0900

    [Silver I] Title: 회의실 배정, Time: 272 ms, Memory: 53948 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -2,17 +2,16 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-
-timetable = []
+arr = []
 for _ in range(n):
-    a, b = map(int, input().split())
-    timetable.append([a, b])
-timetable.sort(key=lambda x: (x[1], x[0]))
+    start, end = map(int, input().split())
+    arr.append([start, end])
+arr.sort(key=lambda x: (x[1], x[0]))
 
-count = 1
-end = timetable[0][1]
+answer = 1
+start, end = arr[0]
 for i in range(1, n):
-    if end <= timetable[i][0]:
-        end = timetable[i][1]
-        count += 1
-print(count)
+    if end <= arr[i][0]:
+        end = arr[i][1]
+        answer += 1
+print(answer)
