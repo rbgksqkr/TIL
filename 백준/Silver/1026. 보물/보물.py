dd3abf4ca6b5395cfcf8c9d7eb672bf405d3e794
Gyuhan Park <rbgks1937@naver.com>
2024-01-22 13:45:18 +0900

    [Silver IV] Title: 보물, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+a.sort()
+b.sort(reverse=True)
+
+result = 0
+for i in range(n):
+    result += a[i]*b[i]
+print(result)
