4cb0b94d98b9832a913a21f5a3e456bd4d832c4f
Gyuhan Park <rbgks1937@naver.com>
2024-03-29 00:01:44 +0900

    [Silver IV] Title: 차집합, Time: 640 ms, Memory: 217596 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.py
+++ 백준/Silver/1822. 차집합/차집합.py
@@ -5,12 +5,28 @@ n, m = map(int, input().split())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
-a = set(a)
-b = set(b)
-diff = list(a-b)
+a.sort()
+b.sort()
 
-diff.sort()
-print(len(diff))
 
-for i in diff:
+def binary_search(start, end, target, arr):
+    while start <= end:
+        mid = (start + end) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return -1
+
+
+answer = []
+for idx, i in enumerate(a):
+    temp = binary_search(0, m-1, i, b)
+    if temp == -1:
+        answer.append(a[idx])
+print(len(answer))
+for i in answer:
     print(i, end=' ')
