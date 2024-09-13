f3d3ad18927714aa52bde572f3a7480962e3cc47
Gyuhan Park <rbgks1937@naver.com>
2024-09-14 01:49:04 +0900

    [Gold V] Title: 집합의 표현, Time: 264 ms, Memory: 76584 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,31 +1,31 @@
 import sys
+sys.setrecursionlimit(1000000) # 재귀 깊이 제한 늘리기
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
-def find(a):
-  if a == parent[a]:
-    return a
+n, m = map(int, input().split())
+parent = [i for i in range(n + 1)] # 자기 자신을 부모로 갖는 n + 1개 집합
 
-  parent[a] = find(parent[a])
-  return parent[a]
+# 찾기 연산(같은 집합에 속하는지 확인하기 위한 함수)
+def find_parent(x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent[x])
+    return parent[x]
 
-def union(a, b):
-  x, y = find(a), find(b)
-
-  if x > y:
-    parent[x] = y
-  elif x < y:
-    parent[x] = y
-
-n, m = map(int,input().split())
-parent = [i for i in range(n+1)]
-
-for i in range(m):
-  op, a, b = map(int,input().split())
-  if op == 1:
-    if find(a) == find(b):
-      print('yes')
+# 합집합 연산(두 집합을 합치기 위한 함수)
+def union_parent(a, b):
+    a = find_parent(a)
+    b = find_parent(b)
+    if a < b: # 값이 더 작은 쪽을 부모로
+        parent[b] = a
     else:
-      print('no')
-  else:
-    union(a,b)
+        parent[a] = b
+        
+for _ in range(m):
+    opr, a, b = map(int, input().split())
+    if opr == 0:
+        union_parent(a, b)
+    else:
+        if find_parent(a) == find_parent(b):
+            print("YES")
+        else:
+            print("NO")
