3f4ac379d9b50468b8e20ec226b7638c66b60651
Gyuhan Park <rbgks1937@naver.com>
2024-10-19 15:28:14 +0900

    [Silver III] Title: 게임, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1072. 게임/게임.py
@@ -0,0 +1,29 @@
+# 1 ≤ X ≤ 1,000,000,000
+# 0 ≤ Y ≤ X
+# z = int((y / x) * 100)
+
+# next_z = int((y + k / x + k) * 100)
+# if z !== next_z: 캐치
+# FIXME: y와 x가 같을 때 0이 되는 문제 발생. z=(100*y)//x.
+
+import sys
+input = sys.stdin.readline
+
+x, y = map(int, input().split())
+
+# z = int((y / x) * 100)
+z = (100*y)//x
+start, end = 0, 1000000000
+answer = -1
+
+while start <= end:
+    mid = (start + end) // 2
+    result = int(((y + mid) / (x + mid)) * 100)
+    if result > z:
+        answer = mid
+        end = mid - 1
+    else:
+        start = mid + 1
+
+
+print(answer)
