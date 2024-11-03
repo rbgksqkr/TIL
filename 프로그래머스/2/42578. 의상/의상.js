d8ff389838a6262bd6059679f6b0df0624b11f0a
Gyuhan Park <rbgks1937@naver.com>
2024-11-04 02:00:39 +0900

    [level 2] Title: 의상, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,21 +1,28 @@
-// 종류별 1가지만 착용
-// 의상 종류별로 분리
-// 1개씩 착용 + 가짓수 곱하기 -> 모든 옷을 다 입을 필요 없음 -> 해당 옷을 안입는 경우도 포함
+// TODO: 서로 다른 옷의 조합의 수
+
+// 각 종류별로 최대 1가지 의상만 가능
+// 1개만 입는 것도 되고, 추가로 입는 것도 다른 종류에 포함
+// 아무것도 안입는 경우는 없음
+
+// 1. 종류별로 분류
+// 2. 종류별로 1가지 의상만 가능하니까 안입는 경우 포함하여 +1 해서 곱하기
+// 3. 아무것도 안입는 경우를 제외하기 위해 -1
 
 function solution(clothes) {
     var answer = 1;
-    const kind = {};
+    // 1. 종류별로 분류
+    let obj = clothes.reduce((acc, cur) => {
+        acc[cur[1]] ? acc[cur[1]] += 1 : acc[cur[1]] = 1
+        return acc;
+    }, {})
     
-    clothes.forEach((cloth) => {
-        if(kind[cloth[1]]){
-            kind[cloth[1]] += 1
-        } else {
-            kind[cloth[1]] = 1
-        }
-    });
-        
-    for(const key in kind) {
-        answer *= (kind[key] + 1);		
+    let values = Object.values(obj);
+    
+    // 2. 종류별로 1가지 의상만 가능하니까 안입는 경우 포함하여 +1 해서 곱하기
+    for(value of values) {
+        answer *= value + 1
     }
+    
+    // 3. 아무것도 안입는 경우를 제외하기 위해 -1
     return answer - 1;
 }
