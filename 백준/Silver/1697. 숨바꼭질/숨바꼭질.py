6d369da0002992df240c6bb11714ca0f050d92de
Gyuhan Park <rbgks1937@naver.com>
2024-11-05 01:32:35 +0900

    [Silver I] Title: 숨바꼭질, Time: 128 ms, Memory: 35268 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,26 +1,36 @@
+# 수빈이는 현재 점 N에 있고, 동생은 점 K에 있다. (0 ≤ N ≤ 100,000) (0 ≤ K ≤ 100,000)
+# 걷기 / 순간이동.
+# 만약, 수빈이의 위치가 X일 때 걷는다면 1초 후에 X-1 또는 X+1로 이동하게 된다.
+# 순간이동을 하는 경우에는 1초 후에 2*X의 위치로 이동하게 된다.
+
+# TODO: 수빈이가 동생을 찾을 수 있는 가장 빠른 시간이 몇 초 후인지?
+
+# 1. -1, 1, 2*x 탐색
+# 2. 이동할 때 초를 세기 위해 visited[mx][my] = visited[x][y] + 1
+
 import sys
 from collections import deque
 input = sys.stdin.readline
-
-N, K = map(int, input().split())
-
-graph = [0 for _ in range(100001)]
+n, k = map(int, input().split())
 
 
-def bfs(x):
-  queue = deque([x])
-  while queue:
+queue = deque([n])
+visited = [0 for _ in range(10**5+1)]
+
+
+while queue:
     x = queue.popleft()
-    if x == K:
-      print(graph[x])
-      break
 
-    d = [x-1, x+1, 2*x]
+    if x == k:
+        print(visited[k])
+        break
 
-    for i in range(3):
-      dx = d[i]
-      if 0 <= dx <= 100000 and graph[dx] == 0:
-        graph[dx] = graph[x] + 1
-        queue.append(dx)
+    for i in [x-1, x+1, x*2]:
+        mx = i
 
-bfs(N)
+        if mx < 0 or mx > 10**5:
+            continue
+
+        if not visited[mx]:
+            visited[mx] = visited[x] + 1
+            queue.append(mx)
