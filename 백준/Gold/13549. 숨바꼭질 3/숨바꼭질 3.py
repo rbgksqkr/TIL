fedaa14420835b5ce040cc150ce7f70996d2a29b
Gyuhan Park <rbgks1937@naver.com>
2024-11-09 14:19:19 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 140 ms, Memory: 35660 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,29 +1,49 @@
-from collections import deque
+# 수빈이는 점 N. 동생은 점 K.  (0 ≤ N ≤ 100,000) (0 ≤ K ≤ 100,000)
+# 수빈이는 걷거나 순간이동 가능
+# X일 때 걷는다면 '1초' 후에 X-1 또는 X+1로 이동
+# 순간이동을 하는 경우에는 '0초' 후에 2*X의 위치로 이동
+
+# TODO: 수빈이가 동생을 찾을 수 있는 가장 빠른 시간이 몇 초 후?
+
+# 1. n, k 를 입력받는다.
+# 2. x-1, x+1, 2*x 탐색
+# 3. 2*x를 갈 땐 0초인 것을 고려.
+
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-INF = int(1e9)
-dp = [INF for _ in range(100001)]
+MAX = 100001
+
 n, k = map(int, input().split())
-dp[n] = 0
 
 queue = deque([n])
-visited = [0 for _ in range(100001)]
+visited = [0 for _ in range(MAX)]
 visited[n] = 1
 
 while queue:
-    cur = queue.popleft()
-    if cur == k:
-        print(dp[k])
+    x = queue.popleft()
+
+    if x == k:
+        print(visited[k]-1)
         break
-    for i in [cur * 2, cur-1, cur+1]:
-        if i < 0 or i > 100000 or visited[i]:
+
+    for i in [x*2]:
+        mx = i
+
+        if mx < 0 or mx >= MAX:
             continue
-        if i == cur * 2:
-            dp[i] = dp[cur]
-            visited[i] = 1
-            queue.appendleft(i)
-        else:
-            dp[i] = dp[cur] + 1
-            visited[i] = 1
-            queue.append(i)
+
+        if not visited[mx]:
+            visited[mx] = visited[x]
+            queue.append(mx)
+
+    for i in [x-1, x+1]:
+        mx = i
+
+        if mx < 0 or mx >= MAX:
+            continue
+
+        if not visited[mx]:
+            visited[mx] = visited[x] + 1
+            queue.append(mx)
