84777e3319d4ec70c4079a419712aad9ee454ba8
Gyuhan Park <rbgks1937@naver.com>
2024-11-15 16:27:17 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.68 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -0,0 +1,49 @@
+# 2 <= n <= 100.
+# 정점(n), 간선 정보(wires) 제공
+# 현재의 전력망 네트워크를 2개로 분할
+
+# TODO: 전선들 중 하나를 끊어서 두 전력망이 가지고 있는 송전탑 개수의 차이(절대값) 구하기
+
+# 1. 인접 리스트 형태로 간선들을 저장한다.
+# 2. 전선을 하나 끊는다.
+# 3. 끊은 그래프에서 두 그래프의 개수 차이를 계산한다.
+# 4. 두 그래프의 개수 차이가 최솟값을 구한다.
+from collections import deque
+
+def check(start, end, visited, graph):
+    visited[start] = 1
+    visited[end] = 1
+    queue = deque([end])
+    
+    count = 1
+    while queue:
+        x = queue.popleft()
+        
+        for i in graph[x]:
+            if not visited[i]:
+                visited[i] = 1
+                count += 1
+                queue.append(i)
+        
+    return count
+        
+def solution(n, wires):
+    answer = -1
+    graph = [[] for _ in range(n+1)]
+    
+    # 1. 인접 리스트 형태로 간선들을 저장한다.
+    for wire in wires:
+        a, b = wire
+        graph[a].append(b)
+        graph[b].append(a)
+        
+    answer = n
+    for wire in wires:
+        visited = [0 for _ in range(n+1)]
+        a, b = wire
+        res = check(a, b, visited, graph) # [a, b] 에 연결된 송전탑 개수
+        alpha = abs(n - res) # [a, b] 에 연결 안된 송전탑 개수 
+        result = abs(res - alpha) # 두 그래프의 개수 차이
+        answer = min(answer, result) # 두 그래프의 개수 차이의 최솟값을 구한다.
+    
+    return answer
