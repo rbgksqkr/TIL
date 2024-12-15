dbf7e25e864eb46c1b2ebd3caaed137f458a741e
Gyuhan Park <rbgks1937@naver.com>
2024-12-15 16:31:32 +0900

    [Gold V] Title: 트리, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.py
+++ 백준/Gold/1068. 트리/트리.py
@@ -9,43 +9,6 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-# inputData = map(int, input().split())
-# target = int(input())
-
-# tree = {}
-
-# for i in range(n):
-#     tree[i] = []
-
-# for idx, data in enumerate(inputData):
-#     # -1 이면 만들고 빈 배열
-#     # -1 아닌데 없으면 만들고 추가
-#     # 있으면 추가
-#     if not tree.get(data):
-#         if data != -1:
-#             tree[data] = [idx]
-#     else:
-#         tree[data].append(idx)
-
-# # target 노드 하위로 제거하기
-
-
-# def deleteTree(root):
-#     for i in tree[root]:
-#         if tree[i]:
-#             deleteTree(i)
-#         del tree[i]
-
-
-# deleteTree(target)
-# del tree[target]
-
-# # # 리프 노드 개수 세기
-# answer = 0
-# for v in tree.values():
-#     if len(v) == 0:
-#         answer += 1
-# print(answer)
 
 # 1. tree 구조로 데이터 저장하기
 tree = [[] for _ in range(n)]
