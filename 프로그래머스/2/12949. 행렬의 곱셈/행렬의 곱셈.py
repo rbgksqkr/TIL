991301b026eaa3f0929742279b31552d258f37cb
Gyuhan Park <rbgks1937@naver.com>
2024-12-27 04:25:24 +0900

    [level 2] Title: 행렬의 곱셈, Time: 100.44 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -3,8 +3,8 @@ def solution(arr1, arr2):
     m = len(arr1[0])
     k = len(arr2[0])
     answer = [[0]*k for _ in range(n)]
+    
     for i in range(n): 
-        lists = []
         for j in range(len(arr2[0])): 
             for k in range(m): 
                 answer[i][j] += arr1[i][k] * arr2[k][j]
