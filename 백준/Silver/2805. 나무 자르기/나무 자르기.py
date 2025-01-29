2e0e7e3e0277f70e647c80f8593519ea725185c1
Gyuhan Park <rbgks1937@naver.com>
2025-01-30 01:09:33 +0900

    [Silver II] Title: 나무 자르기, Time: 2196 ms, Memory: 149248 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,36 +1,37 @@
-# 4 7
-# 20 15 10 17
-# 절단기에 설정할 수 있는 높이?
-# 10 15 17 20
-# maxHeight = 20
-# x = 15
-# -5 0 2 5 -> 7
-# x = 10
-# 0 5 7 10 -> 22
-# ------
-# 이진 탐색의 원리상 start와 end가 수렴 
-# 최종적으로 start와 end가 같은 값을 가리킴
-# 그러나 start와 end를 설정할 때 (start + end) // 2 연산을 수행 
-# 가운데 값을 구하는 것이 아니라 더 작은 값인 start를 사용하여 연산을 진행
-# 따라서, 최종적으로 start와 end가 같은 값이 되면 그 값이 최댓값이 아니라 최댓값보다 1 작은 값
+def solve(N, M, trees):
+    # 이진 탐색을 위한 시작점과 끝점
+    start, end = 0, max(trees)
 
-import sys
-input = sys.stdin.readline
-
-n, m = map(int, input().split())
-data = list(map(int, input().split()))
-data.sort()
-
-start, end = 0, data[-1]
-while start <= end:
-    mid = (start + end) // 2
     result = 0
-    for i in data:
-        if i - mid > 0:
-            result += (i-mid)
+    while start <= end:
+        mid = (start + end) // 2
 
-    if result >= m:
-        start = mid + 1
-    elif result < m:
-        end = mid - 1
-print(end)
+        # 잘려서 가져갈 수 있는 나무의 길이를 계산
+        total = 0
+        for tree in trees:
+            if tree > mid:
+                total += tree - mid
+
+        # 충분한 나무(M 이상)를 얻을 수 있으면
+        # 절단기의 높이를 더 높일 수 있는지 확인해야 함.
+        if total >= M:
+            result = mid  # 일단 현재 mid 값은 유효하므로 기록
+            start = mid + 1
+        else:
+            # 나무가 부족하므로 더 낮게 잘라야 함
+            end = mid - 1
+
+    return result
+
+if __name__ == '__main__':
+    import sys
+    input = sys.stdin.readline
+
+    # 예시 입력
+    # 첫째 줄: 나무의 수 N, 필요한 나무 길이 M
+    # 둘째 줄: 나무의 높이 리스트
+    N, M = map(int, input().split())
+    trees = list(map(int, input().split()))
+    
+    answer = solve(N, M, trees)
+    print(answer)
