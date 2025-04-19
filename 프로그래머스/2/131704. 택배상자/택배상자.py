3d198e2612eba534ba7d5fa343928641470a6090
박규한 <rbgks1937@naver.com>
2025-04-19 22:47:52 +0900

    [level 2] Title: 택배상자, Time: 266.61 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -1,18 +1,17 @@
 # 택배 크기 동일. 1-n번 상자 오름차순으로 일렬 배열.
 # 맨 앞에 놓인 상자 != 트럭에 실어야 하는 순서 -> 보조 컨테이너 벨트 (맨 마지막 추가 상자만 뺄 수 있음)
-
-from collections import deque
-
 def solution(order):
     answer = 0
     
-    belt = []
-    idx = 0
-    for box in range(1, len(order) + 1):
-        belt.append(box)
-        while (belt) and (belt[-1] == order[idx]):
+    belts = []
+    target = 0
+    
+    for i in range(1, (len(order) + 1)):
+        belts.append(i)
+        
+        while belts and belts[-1] == order[target]:
+            target += 1
             answer += 1
-            idx += 1
-            belt.pop()
+            belts.pop()
 
     return answer
