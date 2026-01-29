9d747dc2e45490db53b0a77f7c1e2250d6a6c021
박규한 <rbgks1937@naver.com>
2026-01-29 23:57:10 +0900

    [Silver IV] Title: 동전 0, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -2,12 +2,17 @@ import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-coins = []
-for _ in range(n):
-    coins.append(int(input()))
 
-count = 0
-for i in range(n-1, -1, -1):
-    count += k // coins[i]
-    k %= coins[i]
-print(count)
+prices = []
+for _ in range(n):
+    prices.append(int(input()))
+
+
+prices = reversed(prices)
+
+answer = 0
+for price in prices:
+    answer += k // price
+    k = k % price
+
+print(answer)
